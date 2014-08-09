6195dadde0eb89307270316294ab833b979e9fb1
olszewm1 <gu153EYuk>
2014-08-09 21:16:13 +0200

    cache fix, seriously it works this time

--- src/main/groovy/com/ofg/microservice/github/GithubConfig.groovy
+++ src/main/groovy/com/ofg/microservice/github/GithubConfig.groovy
@@ -2,8 +2,6 @@ package com.ofg.microservice.github
 
 import groovy.transform.TypeChecked
 import net.sf.ehcache.config.CacheConfiguration
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.CacheManager
 import org.springframework.cache.ehcache.EhCacheCacheManager
 import org.springframework.context.annotation.Bean
@@ -15,33 +13,26 @@ class GithubConfig {
 
     public static final String GITHUB_URL = "https://api.github.com/"
 
-//    @Bean
-//    @Autowired
-//    Twitter twitter(@Value('${twitter.id}') String id, @Value('${twitter.secret}') String secret) {
-//        return new TwitterTemplate(id, secret)
-//    }
-
     @Bean
-    CacheManager orgsCacheManager() {
-        CacheConfiguration cacheConfiguration = new CacheConfiguration()
-        cacheConfiguration.setName("orgs")
-        cacheConfiguration.setMemoryStoreEvictionPolicy("LRU")
-        cacheConfiguration.setMaxEntriesLocalHeap(10000)
-        cacheConfiguration.setTimeToLiveSeconds(60 * 10)
+    CacheManager cacheManager() {
+        CacheConfiguration orgsCacheConfiguration = new CacheConfiguration()
+        orgsCacheConfiguration.setName("orgs")
+        orgsCacheConfiguration.setMemoryStoreEvictionPolicy("LRU")
+        orgsCacheConfiguration.setMaxEntriesLocalHeap(1000)
+        orgsCacheConfiguration.setTimeToLiveSeconds(60 * 10)
+
+        CacheConfiguration reposCacheConfiguration = new CacheConfiguration()
+        reposCacheConfiguration.setName("repos")
+        reposCacheConfiguration.setMemoryStoreEvictionPolicy("LRU")
+        reposCacheConfiguration.setMaxEntriesLocalHeap(1000)
+        reposCacheConfiguration.setTimeToLiveSeconds(60 * 10)
+
         net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration()
-        config.addCache(cacheConfiguration)
-        return new EhCacheCacheManager( net.sf.ehcache.CacheManager.newInstance(config) )
+
+        config.addCache(orgsCacheConfiguration)
+        config.addCache(reposCacheConfiguration)
+
+        return new EhCacheCacheManager(net.sf.ehcache.CacheManager.newInstance(config))
     }
 
-    @Bean
-    CacheManager reposCacheManager() {
-        CacheConfiguration cacheConfiguration = new CacheConfiguration()
-        cacheConfiguration.setName("repos")
-        cacheConfiguration.setMemoryStoreEvictionPolicy("LRU")
-        cacheConfiguration.setMaxEntriesLocalHeap(10000)
-        cacheConfiguration.setTimeToLiveSeconds(60 * 10)
-        net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration()
-        config.addCache(cacheConfiguration)
-        return new EhCacheCacheManager( net.sf.ehcache.CacheManager.newInstance(config) )
-    }
 }
